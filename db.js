7522c64d84090c721fd64f9ea378113770f692f8
Connor Hughes <cj.hughes.dev@gmail.com>
2022-01-26 20:00:10 -0500

    blah

--- db.js
+++ db.js
@@ -4,7 +4,8 @@ const {Sequelize} = require("sequelize")
 // const db = new Sequelize(`postgres://postgres:${process.env.PASS}@localhost:5432/Project-FMR`) //!before heroku deployment
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect: 'postgres'
+    dialect: 'postgres',
+    ssl: process.env.ENVIORNMENT === 'production'
 })
 
 
